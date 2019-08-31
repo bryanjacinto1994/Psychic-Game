80923fb917cbf62c784dd3f1c592c9eaf54c81c4
Bryan Jacinto <bryan.f.jacinto1994@gmail.com>
2019-08-30 21:07:13 -0700

    via game.js, Created an instruction to follow to easily put codes.

---
+++ assets/javascript/game.js
@@ -0,0 +1,21 @@
+//Make a variable name of "computerChoices" and a value of arrays with all 26 letters in the alphabet.
+
+//Make a variable for each score subjects.
+
+//Make a function for when a user press a key, that specific key is pressed.
+
+//Make a variable computerGuess and a function that randomizes the alphabet letters.
+
+//Make a variable for the user when a key is pressed via document.onkeyup.
+
+//Make if statements for a user and computer that shows which key is pressed on either sides.
+
+//Make if statements where a user's keys is equal to the computers keys.
+
+//Make an else if statement where if the user's keys is not equal to the computer's keys and also attempts count decrease.
+
+//Make a statement for when attempts reaches "0", user loses and score resets.
+//Also a statement for leters that are guessed to be shown and disappear when user=computer.
+
+
+//Put all IDs and Text-Contents here.
